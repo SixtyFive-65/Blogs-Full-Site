8e2a059ae38f174b2271afede64058206e89b2dc
mxolisi <45sabelo@gmail.com>
2024-05-13 18:47:53 +0200

    Blog Post Crud done

--- Blogz/Blogz.Web/Controllers/AdminBlogPostController.cs
+++ Blogz/Blogz.Web/Controllers/AdminBlogPostController.cs
@@ -35,13 +35,13 @@ namespace Blogz.Web.Controllers
         public async Task<IActionResult> Add(AddBlogPostRequest request)
         {
             var tags = await tagRepository.GetAllTagsAsync();
-           
+
             var blogPostrequest = new BlogPost
             {
                 Heading = request.Heading,
                 Author = request.Author,
                 Content = request.Content,
-                FeaturedImageUrl = request.FeaturedImageUrl,    
+                FeaturedImageUrl = request.FeaturedImageUrl,
                 IsVisible = request.IsVisible,
                 PageTitle = request.PageTitle,
                 ShortDescription = request.ShortDescription,
@@ -50,17 +50,94 @@ namespace Blogz.Web.Controllers
                 PublishDate = request.PublishDate
             };
 
-            var blogPost = await blogPostRepository.CreateAsync(blogPostrequest);   
+            var blogPost = await blogPostRepository.CreateAsync(blogPostrequest);
 
-            return RedirectToAction("Add");
+            return RedirectToAction("GetAllBlogPosts");
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllBlogPosts() 
+        public async Task<IActionResult> GetAllBlogPosts()
         {
             var blogPosts = await blogPostRepository.GetAllAsync();
 
             return View(blogPosts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid blogId)
+        {
+            var blogpostResponse = await blogPostRepository.GetByIdAsync(blogId);
+
+            var tags = await tagRepository.GetAllTagsAsync();
+
+            if (blogpostResponse != null)
+            {
+                if (blogpostResponse != null)
+                {
+                    var blogPost = new EditBlogPostRequest
+                    {
+                        Id = blogpostResponse.Id,
+                        Author = blogpostResponse.Author,
+                        Heading = blogpostResponse.Heading,
+                        Content = blogpostResponse.Content,
+                        ShortDescription = blogpostResponse.ShortDescription,
+                        PublishDate = blogpostResponse.PublishDate,
+                        IsVisible = blogpostResponse.IsVisible,
+                        FeaturedImageUrl = blogpostResponse.FeaturedImageUrl,
+                        UrlHandle = blogpostResponse.UrlHandle,
+                        PageTitle = blogpostResponse.PageTitle,
+                        Tags = tags.Select(p => new SelectListItem { Value = p.Id.ToString(), Text = p.Name }),
+                        SelectedTags = blogpostResponse.Tags.Select(p => p.Id.ToString()).ToArray(),
+                    };
+
+                    return View(blogPost);
+                }
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditBlogPostRequest request)
+        {
+            var tags = await tagRepository.GetAllTagsAsync();
+
+            var blogPost = new BlogPost
+            {
+                Id = request.Id,
+                Author = request.Author,
+                Heading = request.Heading,
+                Content = request.Content,
+                ShortDescription = request.ShortDescription,
+                PublishDate = request.PublishDate,
+                IsVisible = request.IsVisible,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                UrlHandle = request.UrlHandle,
+                PageTitle = request.PageTitle,
+                Tags = tags.Where(p => request.SelectedTags.Contains(p.Id.ToString())).ToList(), //in a real world situation, only loop through the selected tags
+            };
+
+            var editTag = await blogPostRepository.EditAsync(blogPost);
+
+            if (editTag != null)
+            {
+                return RedirectToAction("Edit", new { blogId = request.Id });
+            }
+
+            return RedirectToAction("Edit", new { blogId = request.Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditBlogPostRequest request)
+        {
+            var deleteResult = await blogPostRepository.DeleteAsync(request.Id);
+
+            if (deleteResult != null)
+            {
+                return RedirectToAction("GetAllBlogPosts");
+            }
+
+            return RedirectToAction("Edit", new { blogId = request.Id });
         }
     }
 }
--- Blogz/Blogz.Web/Repositories/BlogPostRepository.cs
+++ Blogz/Blogz.Web/Repositories/BlogPostRepository.cs
@@ -22,14 +22,41 @@ namespace Blogz.Web.Repositories
             return createBlog;
         }
 
-        public Task<BlogPost?> DeleteAsync(Guid id)
+        public async Task<BlogPost?> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var blogPost = await blogsDbContext.BlogPosts.FindAsync(id);
+
+            blogsDbContext.BlogPosts.Remove(blogPost);
+
+            await blogsDbContext.SaveChangesAsync();    
+
+           return blogPost;
         }
 
-        public Task<BlogPost?> EditAsync(BlogPost editBlog)
+        public async Task<BlogPost?> EditAsync(BlogPost editBlog)
         {
-            throw new NotImplementedException();
+            var existingBlogPost =await blogsDbContext.BlogPosts.FindAsync(editBlog.Id);
+
+            if (existingBlogPost != null)
+            {
+                existingBlogPost.Id = editBlog.Id;
+                existingBlogPost.Author = editBlog.Author;
+                existingBlogPost.Heading = editBlog.Heading;
+                existingBlogPost.Content = editBlog.Content;
+                existingBlogPost.ShortDescription = editBlog.ShortDescription;
+                existingBlogPost.PublishDate = editBlog.PublishDate;
+                existingBlogPost.IsVisible = editBlog.IsVisible;
+                existingBlogPost.Tags = editBlog.Tags;
+                existingBlogPost.FeaturedImageUrl = editBlog.FeaturedImageUrl;
+                existingBlogPost.UrlHandle = editBlog.UrlHandle;
+                existingBlogPost.PageTitle = editBlog.PageTitle;
+             
+                await blogsDbContext.SaveChangesAsync();
+
+                return existingBlogPost;
+            }
+
+            return null;
         }
 
         public async Task<IEnumerable<BlogPost>> GetAllAsync()
@@ -43,7 +70,7 @@ namespace Blogz.Web.Repositories
 
         public async Task<BlogPost?> GetByIdAsync(Guid id)
         {
-            return await blogsDbContext.BlogPosts.FindAsync(id);
+            return await blogsDbContext.BlogPosts.Include(p => p.Tags).FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
