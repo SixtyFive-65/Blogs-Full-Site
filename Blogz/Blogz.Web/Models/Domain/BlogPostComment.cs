40ea6f1b0e2d93713760d8c1f1bd8b30e923728b
mxolisi <45sabelo@gmail.com>
2024-05-22 21:37:59 +0200

    add comments

---
+++ Blogz/Blogz.Web/Models/Domain/BlogPostComment.cs
@@ -0,0 +1,11 @@
+﻿namespace Blogz.Web.Models.Domain
+{
+    public class BlogPostComment
+    {
+        public Guid Id { get; set; }
+        public Guid BlogPostId { get; set; }
+        public Guid UserId { get; set; }
+        public string Comment { get; set; }
+        public DateTime CommentDate { get; set; }
+    }
+}
