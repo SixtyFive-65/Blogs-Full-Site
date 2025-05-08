1bc7656f394fed671b3680b6d35665f639ec5111
Mxolisi Mahlalela <65sabelo@gmail.com>
2025-05-08 19:52:45 +0200

    update database connection

--- Blogz/Blogz.Web/Migrations/BlogsAuthDb/20250508172950_first.Designer.cs
+++ Blogz/Blogz.Web/Migrations/BlogsAuthDb/20250508172950_first.Designer.cs
@@ -4,6 +4,7 @@ using Blogz.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blogz.Web.Migrations.BlogsAuthDb
 {
     [DbContext(typeof(BlogsAuthDbContext))]
-    partial class BlogsAuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250508172950_first")]
+    partial class first
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Blogz/Blogz.Web/Migrations/BlogsAuthDb/20250508172950_first.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogz.Web.Migrations.BlogsAuthDb
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("dea533e1-bb48-46a9-94b5-b8e44c2bde4f"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "ebcba15d-fcf7-4d9d-94d3-d174f89810f3", "AQAAAAIAAYagAAAAEKoRdguuvud7yvAu2kr1tvfI5Aw1/YeM8BHRJQn6X2PYmseBXQ0W/NDf1mRj/g1m+g==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("dea533e1-bb48-46a9-94b5-b8e44c2bde4f"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "850a7937-34c6-4fd0-b1fc-fbef52eb41e9", "AQAAAAIAAYagAAAAEFfM86HNsm0v9DV5RLMonpTSmRMsfhWdVCO6ANvEZqvoFgAdD2kstvnWOtGHWu/8OA==" });
+        }
+    }
+}
